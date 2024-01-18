215aa85acd6835f30dc6c036fdb5125bf67d6dd1
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-01-18 15:14:32 -0300

    first commit

---
+++ CPU.v
@@ -0,0 +1,29 @@
+// Recebe o sinal de clock e reset como entrada
+
+// sinais de controle: 
+// declarar todos os fios do diagrama de blocos
+// eh bom listar e separar para nao se perder depois
+// Deixei alguns que da pra acompanhar na monitoria como base
+
+// sinais de escrita
+//         .
+//         .
+//         .
+
+// sinais de escolha de fonte de dados
+//         .
+//         .
+//         .
+
+// sinais de escolha de endereco
+//         .
+//         .
+//         .
+
+// portas logicas
+//         .
+//         .
+//         .
+
+
+// modulos 
