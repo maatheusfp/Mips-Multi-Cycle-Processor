aa9962632c4cb23d4963e3c9756f0358231abcdd
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-01-23 14:59:32 -0300

    just a few wires missing

--- CPU.v
+++ CPU.v
@@ -21,7 +21,7 @@ wire ALUSrcA [1:0];
 wire ALUsrcB [3:0];
 wire IorD [5:0];
 wire RegDst [4:0];
-wire MemtoReg [6:0];
+wire MemtoReg [8:0];
 
 // sinais Ctrl
 wire SHIPTOp3 [1:0];
@@ -68,6 +68,58 @@ wire IR15_0toALUControl [15:0];
 wire IR15_0toShiftLeft [15:0];
 wire IR15_0toMUXRD [15:0];
 
+// Registradores - Falta a quantidade de bits
+wire ReadData1toA [15:0];
+wire ReadData2toB [15:0]; 
+
+// Registrador A
+wire AtoALUSrcA [15:0]; // nao tenho ctz da qtd de bits
+wire AtoEntryCtrl [15:0];
+wire AtoDiv [15:0];
+wire AtoMult [15:0];
+
+// Registrador B 
+wire BtoALUSrcB [15:0];
+wire BtoDiv [15:0];
+wire BtoMult [15:0];
+wire BtoRdcCtrlMUX [31:0];
+wire BtoEntryCtrlMUX [15:0];
+wire BtoWriteDataCtrlMUX [31:0]; 
+
+// ULA
+wire ALUtoALUOut [31:0];
+wire ZerotoBranchMUX;
+wire ZerotoALUOR;
+wire LTtoSignExtend;
+wire LTtoALUOR;
+wire GTtoALUPCOR; // esse ta usando o mesmo fio do diagrama, verificar se ta certo
+wire GTtoBranchMUX;
+
+
+// MDR
+wire MDRtoWordCracker [31:0];
+wire MDRtoSignExtend [7:0];
+wire MDRtoLoadSize [31:0]; // nao tenho ctz
+wire MDRtoMemtoRegMUX [31:0];
+wire MDRtoRdcCtrlMUX [31:0];
+
+// Word Cracker
+wire WCtoWriteDataCtrlMUX; // nao lembro qtos bits
+
+// Load Size
+wire LoadSizetoMemtoRegMUX [31:0];
+
+//Registrador de Deslocamento
+wire RDtoMemtoRegMUX [31:0];
+wire RDtoIorDMUX [31:0];
+
+// DIV
+wire DivtoMultCtrlMUX [31:0];
+wire DivtoDivCtrlMUX [31:0];
+
+// MULT
+wire MulttoMultCtrlMUX [31:0];
+wire MulttoDivCtrlMUX [31:0];
 
 // portas logicas
 //         .
