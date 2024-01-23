4195a9164f536b4ff43c4e8aef07b26cbd696c4d
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-01-23 11:20:23 -0300

    started putting the wires on CPU

--- CPU.v
+++ CPU.v
@@ -1,24 +1,73 @@
-// Recebe o sinal de clock e reset como entrada
+module CPU(
+    input wire clock,
+    input wire reset
+);
 
 // sinais de controle: 
-// declarar todos os fios do diagrama de blocos
-// eh bom listar e separar para nao se perder depois
-// Deixei alguns que da pra acompanhar na monitoria como base
+
+// Reset
+wire Reset;
 
 // sinais de escrita
-//         .
-//         .
-//         .
+wire PCWriteCond;
+wire PCWrite;
+wire MemWrite; 
+wire IRwrite; 
+wire RegWrite; 
 
 // sinais de escolha de fonte de dados
-//         .
-//         .
-//         .
+wire PCSource [3:0];
+wire ALUSrcA [1:0];
+wire ALUsrcB [3:0];
+wire IorD [5:0];
+wire RegDst [4:0];
+wire MemtoReg [6:0];
 
-// sinais de escolha de endereco
-//         .
-//         .
-//         .
+// sinais Ctrl
+wire SHIPTOp3 [1:0];
+wire LoadControl;
+wire BranchControl [3:0];
+wire EPCControl;
+wire ALUOp;
+wire WriteDataCtrl [1:0];
+wire WordCrackerCtrl;
+wire ShiftCtrl [2:0];
+wire EntryCtrl [2:0];
+wire ReduceCtrl [2:0];
+wire HiCtrl;
+wire LoCtrl;
+wire ALUOutCtrl;
+// div e mult ctrl tem em dois lugares (talvez dois diferentes para o mux?)
+
+// excecoes
+wire Ignore [1:0];
+wire Overflow;
+
+// Os proximos fios nomeei a partir de onde saem e pra onde vao
+
+// PC
+wire PCtoMUX [31:0];
+// PCtoShift left (concatena)
+
+//Memory
+wire MemDatatoIR [31:0];
+wire MemDatatoMDR [31:0];
+
+// IR
+wire IR31_26toShiftLeft [5:0];
+wire IR25_21toMUXReg [4:0];
+wire IR25_21toShiftLeft [4:0];
+wire IR25_21toReg [4:0];
+wire IR20_16toShiftLeft [4:0];
+wire IR20_16toReg [4:0];
+wire IR20_16toMUXReg [4:0];
+wire IR10_16toMUXRD [4:0];
+wire IR15_0toSingExtend [15:0];
+wire IR15_0toMUXReg [15:0];
+wire IR15_0toALUControl [15:0];
+wire IR15_0toShiftLeft [15:0];
+wire IR15_0toMUXRD [15:0];
+
 
 // portas logicas
 //         .
