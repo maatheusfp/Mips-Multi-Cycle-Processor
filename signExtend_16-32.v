3dd94c647e1dffcc74f317d67541386ca774d773
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-02-12 19:56:09 -0300

    feat: sign extend 1x32 working

--- signExtend_16-32.v
+++ signExtend_16-32.v
@@ -16,6 +16,35 @@ module signExtend_16x32 (
 
 endmodule
 
-// test:
+// testbench:
+
+// `timescale 1ns / 1ps
+
+// module testbench;
+//     reg [15:0] IR;
+//     wire [31:0] shiftLeft;
+
+//     // Instanciar o módulo que queremos testar
+//     signExtend_16x32 uut (
+//         .IR(IR), 
+//         .shiftLeft(shiftLeft)
+//     );
+
+//     initial begin
+//         // Teste com um número positivo
+//         IR = 16'b0000000000000001;
+//         #10; // Espera 10 unidades de tempo
+//         $display("IR = %b, shiftLeft = %b", IR, shiftLeft);
+
+//         // Teste com um número negativo
+//         IR = 16'b1000000000000000;
+//         #10; // Espera 10 unidades de tempo
+//         $display("IR = %b, shiftLeft = %b", IR, shiftLeft);
+
+//         // Finaliza a simulação
+//         $finish;
+//     end
+// endmodule
+
 // IR = 0000000000000001, shiftLeft = 00000000000000000000000000000001
 // IR = 1000000000000000, shiftLeft = 11111111111111111000000000000000
