a0ab7b4663b37420da99356a0ea4e0133f0ec5db
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-02-13 12:17:14 -0300

    feat: ignore mux and wires ok

---
+++ newComponents/muxes/overflowToControl_unit.v
@@ -0,0 +1,17 @@
+// ignore mux
+// mux 2x1
+module 2x1(
+    input 0,
+    input wire Overflow,
+    input wire ignore
+    output reg IgnoreMUXtoUC;
+);
+    
+      always @(*) begin
+     case (ignore)
+        1'b0: IgnoreMUXtoUC = 0;
+        1'b1: IgnoreMUXtoUC = Overflow;
+     //   default: IgnoreMUXtoUC = 1'b0; // Default value
+     endcase
+    end
+endmodule
