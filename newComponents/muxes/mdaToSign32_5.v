4fbf734e477a8e85734d5eb27795681fc83fe914
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-02-13 12:10:30 -0300

    feat: reduce ctrl mux and wires ok

---
+++ newComponents/muxes/mdaToSign32_5.v
@@ -0,0 +1,17 @@
+// reduce ctrl mux
+// mux 2x1
+module 2x1 (
+    input wire MDRtoRdcCtrlMUX [31:0],
+    input wire BtoRdcCtrlMUX [31:0],
+    input wire RdcCtrl,
+    output reg [31:0] RdcCtrlMUXtoSign32_5
+);
+    
+      always @(*) begin
+     case (RdcCtrl)
+        2'b0: RdcCtrlMUXtoSign32_5 = MDRtoRdcCtrlMUX;
+        2'b1: RdcCtrlMUXtoSign32_5 = BtoRdcCtrlMUX;
+        // default: RdcCtrlMUXtoSign32_5 = 32'b0; // Default value
+     endcase
+    end
+endmodule
