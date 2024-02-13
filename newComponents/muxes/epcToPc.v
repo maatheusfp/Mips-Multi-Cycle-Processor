170cc8eb5b1953c73e0bb3c32467d1cf1de2ffaf
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-02-13 12:25:10 -0300

    feat: pcsource mux and wires ok

---
+++ newComponents/muxes/epcToPc.v
@@ -0,0 +1,21 @@
+// PCSource mux
+// mux 4x1
+module 4x1 (
+    input wire SLtoPCSourceMYX [31:0],
+    input EPCtoPCSourceMUX [31:0],
+    input wire ALUOuttoPCSourceMUX [31:0],
+    input wire SE8_32toPCSource [31:0],
+    input wire PCSource [1:0],
+    output reg [31:0] PCSourceMUXtoPC
+);
+    
+      always @(*) begin
+     case (PCSource)
+        2'b00: PCSourceMUXtoPC = SLtoPCSourceMYX;
+        2'b01: PCSourceMUXtoPC = EPCtoPCSourceMUX;
+        2'b10: PCSourceMUXtoPC = ALUOuttoPCSourceMUX;
+        2'b11: PCSourceMUXtoPC = SE8_32toPCSource;
+        default: PCSourceMUXtoPC = 32'b0; // Default value
+     endcase
+    end
+endmodule
