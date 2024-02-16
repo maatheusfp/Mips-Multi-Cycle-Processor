2701a68816ceb1b16bcddb58bcf2a3961962bd2a
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-02-16 10:54:49 -0300

    Update overflowToControl_unit.v

--- newComponents/muxes/overflowToControl_unit.v
+++ newComponents/muxes/overflowToControl_unit.v
@@ -1,17 +1,16 @@
-// ignore mux
-// mux 2x1
+// Ignore mux - mux 2x1
 module overflowToControl_unit(
-    input 0,
-    input wire Overflow,
-    input wire ignore
-    output reg IgnoreMUXtoUC;
+    input wire [31:0] Overflow,
+    input wire ignore,
+    output reg [31:0] IgnoreMUXtoUC
 );
-    
-      always @(*) begin
-     case (ignore)
-        1'b0: IgnoreMUXtoUC = 0;
-        1'b1: IgnoreMUXtoUC = Overflow;
-     //   default: IgnoreMUXtoUC = 1'b0; // Default value
-     endcase
+
+    always @* begin
+        case (ignore)
+            1'b0: IgnoreMUXtoUC = 32'b0;
+            1'b1: IgnoreMUXtoUC = Overflow;
+            default: IgnoreMUXtoUC = 32'b0; // Valor padrão
+        endcase
     end
+
 endmodule
