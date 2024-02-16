3546cafed8aa71f1d414662fa6d9858ee1a7b01a
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-02-15 22:35:39 -0300

    Update pc.v

--- newComponents/pc.v
+++ newComponents/pc.v
@@ -1,19 +1,19 @@
 module PC(
     input wire [31:0] PCSourceMUXtoPC, 
-    input wire PCWriteORtoPC; 
-    output reg [31:0] PCtoIordMUX;
-    output reg [31:0] PCtoEND;
+    input wire PCWriteORtoPC, 
+    output reg [31:0] PCtoIordMUX,
+    output reg [31:0] PCtoEND
 );
 
     always @(*) begin
-        if (PCWriteORtoPC == 1){
+        if (PCWriteORtoPC == 1) begin
             PCtoIordMUX = PCSourceMUXtoPC; 
             PCtoEND = PCSourceMUXtoPC; 
-        }
-        else {
-            PCtoIordMUX = 16'b0; 
-            PCtoEND = 16'b0;
-        }
+        end
+        else begin
+            PCtoIordMUX = 32'b0; 
+            PCtoEND = 32'b0; // Ajustado para 32 bits
+        end
     end
 
 endmodule
