4a63a8b221ec954b8b069af5c12c1e199957b703
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-02-16 10:57:20 -0300

    Update irToReg.v

--- newComponents/muxes/irToReg.v
+++ newComponents/muxes/irToReg.v
@@ -1,23 +1,22 @@
-// mux 5x1
+// Mux 5x1
 module irToReg(
     input wire [31:0] reg31,
     input wire [31:0] reg29,
     input wire [15:0] IR15_0toMUXReg,
     input wire [4:0] IR20_16toReg,
     input wire [4:0] IR25_21toMUXReg,
-    input wire [2:0] RegDst, // 2-bit input to select between 5 inputs
-    output reg [5:0] RegDstMUXtoReg
+    input wire [2:0] RegDst, // 3-bit input to select between 5 inputs
+    output reg [31:0] RegDstMUXtoReg
 );
 
-    always @*
-    begin
+    always @* begin
         case (RegDst)
-            5'b000: RegDstMUXtoIR = reg31;
-            5'b001: RegDstMUXtoIR = reg29;
-            5'b010: RegDstMUXtoIR = IR15_0toMUXReg;
-            5'b011: RegDstMUXtoIR = IR20_16toReg;
-            5'b100: RegDstMUXtoIR = IR25_21toMUXReg;
-            default: RegDstMUXtoIR = 0; // Default output when RegDst is invalid
+            3'b000: RegDstMUXtoReg = reg31;
+            3'b001: RegDstMUXtoReg = reg29;
+            3'b010: RegDstMUXtoReg = IR15_0toMUXReg;
+            3'b011: RegDstMUXtoReg = IR20_16toReg;
+            3'b100: RegDstMUXtoReg = IR25_21toMUXReg;
+            default: RegDstMUXtoReg = 32'b0; // Valor padrão quando RegDst é inválido
         endcase
     end
 
