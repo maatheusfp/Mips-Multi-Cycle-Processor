0591ca75f7659c1ff9ed71498f2d7b79692ee028
João Teles <127795554+JoaoTeles87@users.noreply.github.com>
2024-02-16 10:52:16 -0300

    Update pcToMem.v

--- newComponents/muxes/pcToMem.v
+++ newComponents/muxes/pcToMem.v
@@ -1,24 +1,22 @@
-module pcToMem( // mux IorD (pc -> mem)
-  input wire [31:0] ALUOuttoIorDMUX,
-  input wire [31:0] PCtoIordMUX, 
-  input wire [31:0] reg253, // check what is the function of this register
-  input wire [31:0] reg254, // check what is the function of this register
-  input wire [31:0] reg255, // check what is the function of this register  
-  input wire [31:0] RDtoIorDMUX, // is it 32 bits or 5 bits?
-  input wire [2:0] IorD, // 3-bit input to select between 6 inputs
-  output reg [31:0] IorDMUXtoMem
+module pcToMem(
+    input wire [31:0] PCSource,
+    input wire [31:0] reg253,
+    input wire [31:0] reg254,
+    input wire [31:0] reg255,
+    input wire [31:0] RDtoIorDMUX,
+    input wire [2:0] IorD,
+    output reg [31:0] IorDMUXtoMem
 );
 
-always @* // changes every time one of the inputs changes
-begin 
-  case (IorD) // case statement to select the correct input
-    3'b000: IorDMUXtoMem = PCSource; // If IorD is 000, IorDMUXtoMem = PCSource
-    3'b001: IorDMUXtoMem = reg253; // If IorD is 001, IorDMUXtoMem = reg253
-    3'b010: IorDMUXtoMem = reg254; // If IorD is 010, IorDMUXtoMem = reg254
-    3'b011: IorDMUXtoMem = reg255; // If IorD is 011, IorDMUXtoMem = reg255
-    3'b100: IorDMUXtoMem = RDtoIorDMUX; // If IorD is 100, IorDMUXtoMem = RDtoIorDMUX
-    default: IorDMUXtoMem = 32'b0; // Default case if IorD is invalid -> 111, 101, 110
-  endcase
+always @* begin
+    case (IorD)
+        3'b000: IorDMUXtoMem = PCSource;
+        3'b001: IorDMUXtoMem = reg253;
+        3'b010: IorDMUXtoMem = reg254;
+        3'b011: IorDMUXtoMem = reg255;
+        3'b100: IorDMUXtoMem = RDtoIorDMUX;
+        default: IorDMUXtoMem = 32'b0; // Default case for invalid IorD values
+    endcase
 end
 
 endmodule
