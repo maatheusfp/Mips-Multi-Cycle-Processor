e1cc17f53862be94aa32edb8e725c6f8df37d656
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-02-18 12:28:41 -0300

    fixing

--- newComponents/muxes/pcToMem.v
+++ newComponents/muxes/pcToMem.v
@@ -1,9 +1,7 @@
 module pcToMem(
     input wire [31:0] PCSource,
-    input wire [31:0] reg253,
-    input wire [31:0] reg254,
-    input wire [31:0] reg255,
     input wire [31:0] RDtoIorDMUX,
+    input wire [31:0] ALUOut,
     input wire [2:0] IorD,
     output reg [31:0] IorDMUXtoMem
 );
@@ -11,10 +9,11 @@ module pcToMem(
 always @* begin
     case (IorD)
         3'b000: IorDMUXtoMem = PCSource;
-        3'b001: IorDMUXtoMem = reg253;
-        3'b010: IorDMUXtoMem = reg254;
-        3'b011: IorDMUXtoMem = reg255;
+        3'b001: IorDMUXtoMem = 32'b11111101; // 253
+        3'b010: IorDMUXtoMem = 32'b11111110; // 254
+        3'b011: IorDMUXtoMem = 32'b11111111;; // 255
         3'b100: IorDMUXtoMem = RDtoIorDMUX;
+        3'b101: IorDMUXtoMem = ALUOut;
         default: IorDMUXtoMem = 32'b0; // Default case for invalid IorD values
     endcase
 end
--- newComponents/muxes/sign32_5ToReg_desloc.v
+++ newComponents/muxes/sign32_5ToReg_desloc.v
@@ -3,16 +3,15 @@
 // Shift control mux - mux 3x1
 module sign32_5ToReg_desloc(
     input wire [4:0] se32x5ToShift_ctrlMUX,
-    input wire [31:0] reg16,
     input wire [4:0] IR10_6toMUXShiftCtrl,
     input wire [1:0] ShiftCtrl,
-    output reg [15:0] ShiftCtrlMUXtoRD
+    output reg [4:0] ShiftCtrlMUXtoRD
 );
     
     always @* begin
         case (ShiftCtrl)
             2'b00: ShiftCtrlMUXtoRD = se32x5ToShift_ctrlMUX;
-            2'b01: ShiftCtrlMUXtoRD = reg16[15:0]; // Considerando apenas os bits [15:0] de reg16
+            2'b01: ShiftCtrlMUXtoRD = 5'b10000; 
             2'b10: ShiftCtrlMUXtoRD = IR10_6toMUXShiftCtrl;
             default: ShiftCtrlMUXtoRD = 16'b0; // Valor padrão
         endcase
