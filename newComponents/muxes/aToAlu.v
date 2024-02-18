27e8038cf4e7996f9a1097d82ada412db4185d15
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-02-18 00:54:08 -0300

    fixing

--- newComponents/muxes/aToAlu.v
+++ newComponents/muxes/aToAlu.v
@@ -8,8 +8,8 @@ module aToAlu(
 
   always @(*) begin
     case (ALUSrcA)
-      1'b0: ALUSrcAMUXtoALU = ALUSrcAMUXtoALU;
-      1'b1: ALUSrcAMUXtoALU = PCtoMUX;
+      1'b0: ALUSrcAMUXtoALU = PCtoMUX;
+      1'b1: ALUSrcAMUXtoALU = ALUSrcAMUXtoALU;
       default: ALUSrcAMUXtoALU = 32'b0; // Default value
     endcase
   end
