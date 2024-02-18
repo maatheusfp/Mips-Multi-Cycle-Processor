a759d2c9dd686329bf114128089ae4d81ad36297
Matheus Pinheiro <matheus.pinheirofer@gmail.com>
2024-02-18 11:44:15 -0300

    fixing

--- newComponents/muxes/wcToMem.v
+++ newComponents/muxes/wcToMem.v
@@ -1,15 +1,15 @@
 // mux 2x1
 module wcToMem(
-    input [31:0] WCtoWriteDataCtrlMUX,
-    input [31:0] BtoWriteDataCtrlMUX,
+    input wire [31:0] WCtoWriteDataCtrlMUX,
+    input wire [31:0] BtoWriteDataCtrlMUX,
     input wire WriteDataCtrl,
     output reg [31:0] WriteDataCtrlMUXtoMem
 );
 
   always @(*) begin
     case (WriteDataCtrl)
-      2'b0: WriteDataCtrlMUXtoMem = WCtoWriteDataCtrlMUX;
-      2'b1: WriteDataCtrlMUXtoMem = BtoWriteDataCtrlMUX;
+      1'b0: WriteDataCtrlMUXtoMem = WCtoWriteDataCtrlMUX;
+      1'b1: WriteDataCtrlMUXtoMem = BtoWriteDataCtrlMUX;
       default: WriteDataCtrlMUXtoMem = 32'b0; // Default value
     endcase
   end
