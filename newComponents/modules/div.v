5dcfdba4bd77f71437a00bc16e77107fa9752b0a
Gabriel Rocha <120466775+gabrielrochass@users.noreply.github.com>
2024-02-18 23:47:45 -0300

    feat: div

--- newComponents/modules/div.v
+++ newComponents/modules/div.v
@@ -1,60 +1,80 @@
 module div (
-    input wire clock,
-    input wire reset,
-    input wire [31:0] dividend,
-    input wire [31:0] divisor,
-    output reg [31:0] hi_div,
-    output reg [31:0] lo_div,
-    output reg division_by_zero
+  input wire clk, reset, start,
+  input wire [31:0] dividendo,
+  input wire [31:0] divisor,
+  output reg divzero,
+  output reg [31:0] Hi, Lo
 );
 
-reg [31:0] remainder;
-reg [31:0] quotient;
-reg [31:0] temp;
-reg [5:0] counter;
+  reg [63:0] div; 
+  reg [63:0] resto; 
+  reg [31:0] quociente;
+  reg [5:0] counter;
 
-always @(posedge clock or posedge reset) begin
-    if (reset) begin
-        hi_div <= 32'b0;
-        lo_div <= 32'b0;
-        remainder <= 32'b0;
-        quotient <= 32'b0;
-        division_by_zero <= 1'b0;
-        counter <= 6'b0;
-    end
-    else begin
-        if (divisor == 32'b0) begin
-            hi_div <= 32'b0;
-            lo_div <= 32'b0;
-            division_by_zero <= 1'b1;
+  always @(posedge clk) begin
+    if (reset == 1'b1 || start == 1'b1) begin
+      if (divisor == 32'd0) begin 
+        divzero = 1'b1;
+      end 
+      else begin
+
+        if (dividendo[31] == 1'b1) begin
+          resto[31:0] = ~dividendo + 1'b1; 
+        end else begin
+          resto[31:0] = dividendo;
         end
-        else begin
-
-            if (counter == 0) begin
-                remainder <= dividend;
-                quotient <= 32'b0;
-                division_by_zero <= 1'b0;
-            end
-
-            else if (counter < 32) begin
-                temp = {remainder[31], remainder} - (divisor << (31 - counter));
-                
-                if (temp[31] == 0) begin
-                    remainder <= temp;
-                    quotient[counter] <= 1'b1;
-                end
-                else begin
-                    quotient[counter] <= 1'b0;
-                end
-                counter <= counter + 1;
-            end
-            
-            hi_div <= remainder;
-            lo_div <= quotient;
-            counter <= 6'b0;
-
+  
+        if (divisor[31] == 1'b1) begin
+          div[63:32] = ~divisor + 1'b1; 
+        end else begin
+          div[63:32] = divisor;
         end
-    end
-end
 
+        div[31:0] = 32'd0;
+        resto[63:32] = 32'd0;
+        quociente = 32'd0;
+        Hi = 32'd0;
+        Lo = 32'd0;
+        divzero = 1'b0;
+        counter = 6'd33;
+        
+      end
+      
+    end else begin 
+      if (divzero != 1'b1 && counter != 6'd0) begin
+        resto = resto - div;
+        
+        if (resto[63] == 1'b1) begin
+          resto = resto + div;
+          quociente = {quociente[30:0], 1'b0};
+        end else begin
+          quociente = {quociente[30:0], 1'b1};
+        end
+
+        div = div >> 1'b1;
+
+        counter = counter - 6'd1;
+
+        if (counter == 6'd0) begin 
+          if (divisor[31] != dividendo[31]) begin 
+            if (divisor[31] == 1'b1) begin 
+              Hi = resto[31:0];
+              Lo = ~quociente + 1'b1; 
+            end else begin 
+              Hi = ~resto[31:0] + 1'b1; 
+              Lo = ~quociente + 1'b1; 
+            end
+          end else begin 
+            if (divisor[31] == 1'b1) begin 
+              Hi = ~resto[31:0] + 1'b1;
+              Lo = quociente;
+            end else begin 
+              Hi = resto[31:0];
+              Lo = quociente;
+            end
+          end
+        end
+      end
+    end
+  end
 endmodule
